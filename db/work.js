e8d966654adbbff35e6f2b3cc0ca6d330ddc7a17
pengyan <1580559335@qq.com>
2022-03-26 23:16:18 +0800

    注释假数据

--- db/work.js
+++ db/work.js
@@ -49,7 +49,16 @@ work.pre("update", function (next) {
 
 
 
-let article = mongoose.model("work", work);
+let works = mongoose.model("work", work);
+
+/*假数据*/
+// for (let i=0;i<100;i++){
+//   works.create({
+//     title: `作品%{i+1}`,
+//     intro: '这是作品这是作品这是作品这是作品这是作品这是作品这是作品',
+//     link: '作品链接作品链接作品链接作品链接作品链接',
+//   });
+// }
 
 
-module.exports = article;
+module.exports = works;
